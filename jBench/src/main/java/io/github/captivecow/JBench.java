4e26a772e376e3e8206529d65185cd2c4c2460ab
captivecow <captivecow.dev@gmail.com>
2024-08-15 21:54:19 -0500

    feat: Initial commit

---
+++ jBench/src/main/java/io/github/captivecow/JBench.java
@@ -0,0 +1,13 @@
+package io.github.captivecow;
+
+/**
+ * Hello world!
+ *
+ */
+public class JBench
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
