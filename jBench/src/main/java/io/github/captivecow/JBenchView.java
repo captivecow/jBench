e9dc01030f1e87866113bcf235586439aebc2231
captivecow <captivecow.dev@gmail.com>
2024-08-19 21:34:01 -0500

    feat: Add render hints for speed and dirty delta time on screen

--- jBench/src/main/java/io/github/captivecow/JBenchView.java
+++ jBench/src/main/java/io/github/captivecow/JBenchView.java
@@ -7,12 +7,18 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
+import java.text.DecimalFormat;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class JBenchView implements Runnable {
+
+    private Map<RenderingHints.Key, Object> HINTS;
+    private DecimalFormat df = new DecimalFormat("#.####");
+
 
     private final JFrame frame;
     private final Canvas canvas;
@@ -20,11 +26,16 @@ public class JBenchView implements Runnable {
     private final GridBagLayout layout;
     private final GridBagConstraints constraints;
     private final BottomPanelView bottomPanel;
-    private BufferedImage sprite;
     private final ScheduledExecutorService fpsScheduler =
             Executors.newScheduledThreadPool(1);
     private RenderState state;
     private final Runnable beingRendering;
+    private Sprite sprite;
+    private ArrayList<Sprite> spriteList;
+    private Map<Integer, BufferedImage> imageMap;
+    private long lastTime;
+    private double accumulation = 0;
+    private double lastDelta = 1;
 
     public JBenchView(){
         frame = new JFrame("jBench");
@@ -32,6 +43,8 @@ public class JBenchView implements Runnable {
         layout = new GridBagLayout();
         constraints = new GridBagConstraints();
         bottomPanel = new BottomPanelView();
+        spriteList = new ArrayList<>();
+        imageMap = new HashMap<>();
         beingRendering = this::render;
     }
 
@@ -69,7 +82,31 @@ public class JBenchView implements Runnable {
         canvas.createBufferStrategy(2);
         bufferStrategy = canvas.getBufferStrategy();
 
-        fpsScheduler.scheduleAtFixedRate(beingRendering, 0, 1, TimeUnit.SECONDS);
+        lastTime = System.nanoTime();
+        fpsScheduler.scheduleAtFixedRate(beingRendering, 0, 16, TimeUnit.MILLISECONDS);
+    }
+
+    public void initRenderState(){
+        HINTS = new HashMap<>();
+        HINTS.put(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+        HINTS.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
+        HINTS.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+        HINTS.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+
+        state = new RenderState(new DisplayModeOption(800, 600), new RenderSize(16, 16), 10);
+
+        for (int i = 0; i<state.renderAmount(); i++){
+            int randomX = ThreadLocalRandom.current().nextInt(0, 800 + 1);
+            int randomY = ThreadLocalRandom.current().nextInt(0, 600 + 1);
+
+            Sprite newSprite = new Sprite(
+                    state.renderSize().width(),
+                    state.renderSize().height(),
+                    randomX,
+                    randomY);
+            spriteList.add(newSprite);
+        }
     }
 
     @Override
@@ -104,19 +141,53 @@ public class JBenchView implements Runnable {
             Load image with BITMASK transparency
             https://docs.oracle.com/en/java/javase/17/troubleshoot/java-2d.html#GUID-D5849E59-9EB6-46FA-BA47-EA5C52DFA077
          */
-        sprite = graphicsConfiguration.createCompatibleImage(
+        BufferedImage compatibleImage = graphicsConfiguration.createCompatibleImage(
                 rawImage.getWidth(),
                 rawImage.getHeight(), Transparency.BITMASK
         );
 
-        Graphics2D graphics2D = (Graphics2D) sprite.getGraphics();
+        Graphics2D graphics2D = (Graphics2D) compatibleImage.getGraphics();
         graphics2D.drawImage(rawImage, 0, 0, null);
         graphics2D.dispose();
+
+        imageMap.put(1, compatibleImage);
     }
 
 
     public void render(){
-        System.out.println(SwingUtilities.isEventDispatchThread());
-        System.out.println("Hello world");
+        /*
+            Correct way to render with buffer strategy
+            https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/java/awt/image/BufferStrategy.html#:~:text=The%20BufferStrategy%20class%20represents%20the,buffer%20strategy%20can%20be%20implemented.
+         */
+        do {
+            do {
+
+                long time = System.nanoTime();
+                double currentDelta = (double) (time - lastTime)/1000000000.0;
+                lastTime = time;
+                accumulation += currentDelta;
+
+                Graphics2D g2d = (Graphics2D) bufferStrategy.getDrawGraphics();
+                g2d.clearRect(0, 0, 800, 600);
+                g2d.setRenderingHints(HINTS);
+
+                if(accumulation > 1.0){
+                    lastDelta = currentDelta;
+                    accumulation = 0;
+                }
+
+                for (Sprite sprite: spriteList){
+                    g2d.drawImage(imageMap.get(sprite.getSpriteId()),
+                            (int) sprite.getX(), (int) sprite.getY(),
+                            sprite.getWidth(), sprite.getHeight(), null);
+                }
+                g2d.drawString("FPS: " + df.format(Math.round(1/lastDelta)), 0, 10);
+
+                g2d.dispose();
+            } while (bufferStrategy.contentsRestored());
+            // Display the buffer
+            bufferStrategy.show();
+            // Repeat the rendering if the drawing buffer was lost
+        } while (bufferStrategy.contentsLost());
     }
 }
