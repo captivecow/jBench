61277d817aca4b89d678a95f95ddced8b3c5b94b
captivecow <captivecow.dev@gmail.com>
2024-08-19 23:54:14 -0500

    feat: Start using GUI options within rendering, added resizing of canvas, images, and amount of images on screen

--- jBench/src/main/java/io/github/captivecow/JBenchView.java
+++ jBench/src/main/java/io/github/captivecow/JBenchView.java
@@ -9,17 +9,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class JBenchView implements Runnable {
-
     private Map<RenderingHints.Key, Object> HINTS;
-    private DecimalFormat df = new DecimalFormat("#.####");
-
-
     private final JFrame frame;
     private final Canvas canvas;
     private BufferStrategy bufferStrategy;
@@ -36,6 +29,7 @@ public class JBenchView implements Runnable {
     private long lastTime;
     private double accumulation = 0;
     private double lastDelta = 1;
+    private ConcurrentLinkedQueue<RenderState> viewEvents;
 
     public JBenchView(){
         frame = new JFrame("jBench");
@@ -45,6 +39,7 @@ public class JBenchView implements Runnable {
         bottomPanel = new BottomPanelView();
         spriteList = new ArrayList<>();
         imageMap = new HashMap<>();
+        viewEvents = new ConcurrentLinkedQueue<>();
         beingRendering = this::render;
     }
 
@@ -66,7 +61,7 @@ public class JBenchView implements Runnable {
         bottomPanel.getUpdateRenderButton().addActionListener(e -> updateRenderState());
         constraints.gridx = 0;
         constraints.gridy = 1;
-        constraints.fill = GridBagConstraints.BOTH;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
         frame.add(bottomPanel.getBottomPanel(), constraints);
 
         frame.setIgnoreRepaint(true);
@@ -96,8 +91,8 @@ public class JBenchView implements Runnable {
         state = new RenderState(new DisplayModeOption(800, 600), new RenderSize(16, 16), 10);
 
         for (int i = 0; i<state.renderAmount(); i++){
-            int randomX = ThreadLocalRandom.current().nextInt(0, 800 + 1);
-            int randomY = ThreadLocalRandom.current().nextInt(0, 600 + 1);
+            int randomX = ThreadLocalRandom.current().nextInt(0, 800);
+            int randomY = ThreadLocalRandom.current().nextInt(0, 600);
 
             Sprite newSprite = new Sprite(
                     state.renderSize().width(),
@@ -114,7 +109,8 @@ public class JBenchView implements Runnable {
     }
 
     public void updateRenderState(){
-        state = getRenderState();
+        RenderState updatedState = getRenderState();
+        viewEvents.add(updatedState);
     }
 
     public RenderState getRenderState(){
@@ -152,6 +148,38 @@ public class JBenchView implements Runnable {
         imageMap.put(1, compatibleImage);
     }
 
+    public void update(RenderState newState){
+        if(!newState.displayModeOption().toString().equals(state.displayModeOption().toString())){
+            canvas.setPreferredSize(new Dimension(
+                    newState.displayModeOption().width(),
+                    newState.displayModeOption().height()));
+            frame.pack();
+            frame.revalidate();
+            bottomPanel.getBottomPanel().repaint();
+        }
+        if(!newState.renderSize().toString().equals(state.renderSize().toString())){
+            // Still need random
+            for(Sprite sprite: spriteList){
+                sprite.setWidth(newState.renderSize().width());
+                sprite.setHeight(newState.renderSize().height());
+            }
+        }
+        if(newState.renderAmount() != state.renderAmount()){
+            spriteList = new ArrayList<>();
+            for(int i = 0; i<newState.renderAmount(); i++){
+                int randomX = ThreadLocalRandom.current().nextInt(0, newState.displayModeOption().width());
+                int randomY = ThreadLocalRandom.current().nextInt(0, newState.displayModeOption().height());
+                Sprite newSprite = new Sprite(
+                        newState.renderSize().width(),
+                        newState.renderSize().height(),
+                        randomX,
+                        randomY);
+                spriteList.add(newSprite);
+            }
+        }
+        state = newState;
+    }
+
 
     public void render(){
         /*
@@ -160,11 +188,15 @@ public class JBenchView implements Runnable {
          */
         do {
             do {
-
                 long time = System.nanoTime();
                 double currentDelta = (double) (time - lastTime)/1000000000.0;
                 lastTime = time;
                 accumulation += currentDelta;
+
+                if(!viewEvents.isEmpty()){
+                    RenderState newState = viewEvents.remove();
+                    update(newState);
+                }
 
                 Graphics2D g2d = (Graphics2D) bufferStrategy.getDrawGraphics();
                 g2d.clearRect(0, 0, state.displayModeOption().width(), state.displayModeOption().height());
@@ -180,7 +212,11 @@ public class JBenchView implements Runnable {
                             (int) sprite.getX(), (int) sprite.getY(),
                             sprite.getWidth(), sprite.getHeight(), null);
                 }
-                g2d.drawString("FPS: " + df.format(Math.round(1/lastDelta)), 0, 10);
+
+                g2d.setColor(Color.ORANGE);
+                g2d.fillRect(0, 0, 50, 15);
+                g2d.setColor(Color.BLACK);
+                g2d.drawString("FPS: " + Math.round(1/lastDelta), 4, 11);
 
                 g2d.dispose();
             } while (bufferStrategy.contentsRestored());
